ede2e67c0b1942c308b79e7bdf70198aed9b51c1
andyau1123 <168272978+andyau1123@users.noreply.github.com>
2024-04-27 18:47:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="images/polypng.png">
+	<title>Conference of Building and Sustaining the Momentum of Cross-border E-commerce for the Greater Bay Area</title>
+	<!-- Font Aewsome CSS -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/navbar-fixed-top.css" rel="stylesheet">
+	<!-- Overriding css that is literally copy pasted for the entire site -->
+	<link href="css/main-override.css" rel="stylesheet">
+	<script src="js/ie-emulation-modes-warning.js"></script>
+</head>
+
+<body class="fontfamily">
+	<?php include "components/header.php" ?>
+	<div class="container">
+		<!-- Main component for a primary marketing message or call to action -->
+		<div class="jumbotron">
+			<h2 style="color: #111177;">About this conference</h2>
+			<br />
+			<p style="text-align: justify;color: #000000;">
+				This 2-day conference is approved and funded by the Inter-Institutional Development Scheme (IIDS) project. The IIDS project reference number is UGC/IIDS24/B02/22. The principal investigator of the IIDS project is Dr. Joseph LAU. The IIDS project title is “Building and Sustaining the Momentum of Cross-border E-commerce for the Greater Bay Area”.
+			</p>
+			<p style="text-align: justify;color: #000000;">
+				The emergence and impact of the COVID-19 pandemic have caused the stagnation of global economic development, but also, provided a major opportunity for international trade to advance its digital transformation for the rise of e-commerce sales. Indeed, social distancing has determined the growing tendency toward online consumption, and cross-border e-commerce, especially in the Greater Bay Area (GBA), has become an important way to connect manufacturing with global consumption. In response to this opportunity, there is an acute need to quickly adapt to new supply-demand relationships in the GBA, and thus, this proposal will have presenters who already have expertise in cross-border e-commerce businesses in the GBA in China, and the audience comprises individuals who need this digital expertise. The workshops and conference sessions will highlight the issues of cross-border e-commerce businesses in the GBA. These issues may include a balance in the use of innovative technologies and knowledge management, research knowledge and skills, education and training, regulatory and governance techniques and protocols. The conference would be the first of its kind to address such cross-border e-commerce issues.
+			</p>
+			<p style="text-align: justify;color: #000000;">
+			The project is supported by School of Professional Education and Executive Development of The Hong Kong Polytechnic University (PolyU SPEED), Beijing Normal University - Hong Kong Baptist University United International College, Macao Institute for Tourism Studies, Shenzhen University, The Hang Seng University of Hong Kong, The Hong Kong Polytechnic University, Technological and Higher Education Institute of Hong Kong, Climate Change Adaptation Planning for Port, Transportation Infrastructure, and the Arctic, Hong Kong Sea Transport and Logistics Association, Institute of Seatransport, Hong Kong Maritime Museum, Policy Research Institute of Global Supply Chain, Hong Kong Institute of Engineers - Logistics and Transportation Division, CY Tung International Centre for Maritime Studies, Research Center for Green Business, Hong Kong Cross-Border E-commerce Association, China Maritime Institute, The Federation of Greater Bay International Logistics and Supply Chain Industry, and Hong Kong Association for VR AR.
+			</p>
+			<p style="text-align: justify;color: #000000;">
+				Through this collaboration, this conference will increase the awareness and support from the industries to share ways to facilitate regional economic development and encourage greater economic integration within the GBA via cross-border e-commerce business development. In response, the conference participants will explore case studies of global companies' use of e-commerce, with analyses of the use and impact of e-commerce. The participants will also discuss the benefits of e-commerce, the implementation of e-commerce applications, market opportunities and winning strategies of e-commerce.
+			</p>
+			<br>
+			<hr>
+			<h2 style="color: #111177;">Co-Organisers (in Alphabetical Order)</h2>
+			<br />
+			<ul style="font-size:16px">
+				<li>Beijing Normal University - Hong Kong Baptist University United International College, China</li>
+				<li>Climate Change Adaptation Planning for Port, Transportation Infrastructure, and the Arctic, Canada</li>
+				<li>Hong Kong Sea Transport and Logistics Association, Hong Kong</li>
+				<li>Institute of Seatransport, Hong Kong</li>
+				<li>Macao Institute for Tourism Studies, Macao</li>
+				<li>Shenzhen University, China</li>
+				<li>Technological and Higher Education Institute of Hong Kong, Hong Kong</li>
+				<li>The Hang Seng University of Hong Kong, Hong Kong</li>
+				<li>The Hong Kong Polytechnic University, Hong Kong</li>
+			</ul>
+			<br />
+
+			<br />
+
+		</div> <!-- /container -->
+	</div>
+	<?php include "components/footer.php" ?>
+
+	<!-- Bootstrap core JavaScript
+  ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<!-- dev of 2021: next time pls include components, and placing js at the end of docs is to make sure the DOM tree built first, not for performance. -->
+	<?php include "components/fixedButton_n_scripts.php" ?>
+</body>
+
+</html>
---
+++ speakers.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="images/polypng.png">
+	<title>
+		<?php 
+			include "components/page_title.php";
+			echo "$speakerPage $confName";
+		?>
+	</title>
+	<!-- Font Aewsome CSS -->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/navbar-fixed-top.css" rel="stylesheet">
+	<!-- Overriding css that is literally copy pasted for the entire site -->
+	<link href="css/main-override.css" rel="stylesheet">
+	<script src="js/ie-emulation-modes-warning.js"></script>
+</head>
+
+<style>
+	.speaker_topics {
+		font-family: Georgia, 'Times New Roman', Times, serif;
+		font-style: italic;
+	}
+	.section_heading {
+		border-bottom: 2px solid rgb(14,135,31);
+		padding: 0px 0px 10px 0px;
+		margin-bottom: 30px;
+	}
+	.speaker {
+		margin-bottom: 5rem;
+	}
+	.speaker a{
+		font-weight: bold;
+	}
+</style>
+
+<body class="fontfamily">
+	<?php include "components/header.php" ?>
+	<div class="container">
+		<div class="jumbotron">
+			<!-- PAGE HEADER: Speakers  -->
+			<h2 style="color: #111177;">Conference Speakers</h2><br>
+			
+			<!-- Keynote Speakers - SECTION HEADER -->
+			<h3 class="section_heading">Keynote Speakers</h3>
+
+			<!-- Keynote Speakers - SECTION CONTENT - START  -->
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Sunny_Ho.jpeg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Mr. Sunny Ho (MH JP)</a> <br>
+						Executive Director, The Hong Kong Shippers' Council
+					</p>
+					<blockquote class="speaker_topics">Defining Hong Kong's Role with regards to Cross-border Ecommerce Business</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/So_Kai_Man.jpeg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Mr. SO Kai-man</a> <br>
+						Executive Chairman, Hong Kong Cross-Border E-Commerce Association
+					</p>
+					<blockquote class="speaker_topics">How can we promote Hong Kong to become the Asian hub for green cross-border e-commerce in coordination with the Greater Bay Area?</blockquote>
+				</div>
+			</div>
+		
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Tse_Hing_Ming.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Mr. Tse Hing Min, Andy</a> <br>
+						LL.B., Partner, HK & GBA Lawyer - SUN LAWYERS
+					</p>
+					<blockquote class="speaker_topics">Contemporary Legal Issues in the Greater Bay Area E-Commerce</blockquote>
+				</div>
+			</div>
+					
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/JingJing_YANG.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="https://www.iftm.edu.mo/stm/en/people/faculty-members/index.html?name=jingjing" target="_blank">Dr. Yang Jingjing</a> <br>
+						Assistant Professor, Macao Institute for Tourism Studies
+					</p>
+					<blockquote class="speaker_topics">Cross-border consumption of tourism industry in GBA</blockquote>
+				</div>
+			</div>
+			<!-- Keynote Speakers - SECTION CONTENT - END -->
+
+
+			<!-- Speakers - SECTION HEADER -->
+			<h3 class="section_heading">Speakers</h3>
+			
+			<!-- Speakers - SECTION CONTENT - START -->
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Wilson_Shum.jpeg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Mr. SHUM Wilson Kwok-fung</a> <br>
+						CEO/Co-Founder - SCALE InnoTech
+					</p>
+					<blockquote class="speaker_topics">Exploring Opportunities with Cross-Border E-commerce, Online to Offline Business Models</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/YukMingTang.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Dr. TANG Yuk-ming</a> <br>
+						Senior Teaching Fellow - The Hong Kong Polytechnic University
+					</p>
+					<blockquote class="speaker_topics">The Adoption of Blockchain for Cross-Border E-commerce in the Greater Bay Area</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Michelle.jpeg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Ms CHENG Michelle Shuk-yuen</a> <br>
+						General Manager - Greater Bay Airlines Company Limited
+					</p>
+					<blockquote class="speaker_topics">Emerging Aviation Issues: Cross-border E-commerce Business for the Greater Bay Area</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Paul_Li.jpeg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Mr. LI Paul Ping Hung</a> <br>
+						President - Transland Shipping (1971) Group
+					</p>
+					<blockquote class="speaker_topics">Import Drugs into the Greater Bay Area: Cases on Cross-border E-commerce</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Yang_Huijun.jpeg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Dr. YANG Huijun</a> <br>
+						Assistant Professor - Macao Institute for Tourism Studies
+					</p>
+					<blockquote class="speaker_topics">Smart Technology Supports the Development of Cross-border Consumption in the Catering Industry</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/placeholder.png" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Mr. YEUNG John Kunwoon</a> <br>
+						Director, Bookschwa Technologies
+					</p>
+					<blockquote class="speaker_topics">Twin Engine of Empowering Cross-Border E-commerce Platforms: The Applications of Cloud Computing and Artificial Intelligence (AI)</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Harman_Ng.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Mr. NG Harman Wai-chung</a> <br>
+						Vice Chairman, The Federation of Greater Bay International Logistics and Supply Chain Industry
+					</p>
+					<blockquote class="speaker_topics">The Regional Comprehensive Economic Partnership (RCEP) and China Import/Export Trade Facilitation</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Sio_Hou_In.jpeg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Ms. SIO Sandy Hou-in</a> <br>
+						President, Administrative Council of the Macau Institute for Corporate Social Responsibility in Greater China
+					</p>
+					<blockquote class="speaker_topics">Sustainability and Diversification in Cross-border E-commerce for the Greater Bay Area</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/ZHANG_Ruopiao.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Dr ZHANG Ruopiao</a> <br>
+						Assistant Professor, Macau University of Science and Technology
+					</p>
+					<blockquote class="speaker_topics">The Role of GBA Cooperation in the China Biodiversity Strategy 2030 through the Lens of Cross-border Consumption</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Wong_Ling.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Ms. WONG Ling</a> <br>
+						Founder, 100degreec
+					</p>
+					<blockquote class="speaker_topics">The Effectiveness of Omni channel for Cross-border E-commerce in the Greater Bay Area</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Hung_Man_Ching.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Mr. Alex HUNG, MH</a> <br>
+						Managing Director of Crossover International Co. Ltd. & Charter President of Rotary Club of Peninsula East
+					</p>
+					<blockquote class="speaker_topics">Cross-border E-commerce in the Greater Bay Area</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Kwok_Man_Ki.jpeg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Mr. KWOK Man-ki Philip Kino</a> <br>
+						Founder & CEO of GPEC Holding Limited; President of Hong Kong Cross-Border E-commerce Association
+					</p>
+					<blockquote class="speaker_topics">Cross-border E-Commerce Payment Security and Fraud Prevention</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/John_Yu.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="#" target="_blank">Mr. YU Shing Chung, John</a> <br>
+						Professional Member of Hong Kong Sea Transport and Logistics Association (HKSTLA); Head of Operations, Aramex Hong Kong Limited
+					</p>
+					<blockquote class="speaker_topics">Post COVID Market Dynamics in Cross-border E-commerce</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Wong_Weng_Chou.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="https://www.must.edu.mo/en/fhtm/staff/wongwengchou" target="_blank">Dr. Wong Weng Chou, Jose</a> <br>
+						Associate Professor, Macau University of Science and Technology
+					</p>
+					<blockquote class="speaker_topics">Information Technology of E-commerce in hospitality and tourism industry: development and trends</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Ivy_Shum.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="https://hkavrar.com/" target="_blank">Ms. Ivy Shum</a> <br>
+						President of Hong Kong Association for VR AR
+					</p>
+					<blockquote class="speaker_topics"> Adoption of VR AR in E-commerce of Greater Bay Area</blockquote>
+				</div>
+			</div>
+
+			<div class="row speaker">
+				<div class="col-xs-4 col-sm-4 col-md-2 col-lg-2">
+					<div class="img-rounded" style="box-shadow: 0px 0px 8px 5px rgba(0,0,0,0.2);">
+						<img src="images/speakers/Jones_Ng.jpg" style="width:100%">
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">
+					<p>
+						<a href="https://www.chiwa.hk/training-seminar/" target="_blank">Mr. Jones Ng</a> <br>
+						Director, Chiwa Digital Media Capital Group
+					</p>
+					<blockquote class="speaker_topics">The Promotional Strategies of Cross Border E-Commerce in the Greater Bay Area</blockquote>
+				</div>
+			</div>
+			<!-- Keynote Speakers - SECTION CONTENT - END -->
+
+		</div>
+	</div> <!-- /container -->
+	<?php include "components/footer.php" ?>
+
+	<!-- Bootstrap core JavaScript
+  ================================================== -->
+	<!-- Placed at the end of the document so the pages load faster -->
+	<?php include "components/fixedButton_n_scripts.php" ?>
+</body>
+
+</html>
